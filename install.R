bcd624affb14bb8519d9e05c71041817c58015d3
Aniruddha Belsare <anyadoc@gmail.com>
2020-11-28 01:48:31 -0500

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,1 @@
-install.packages("readr","dplyr","ggplot2","tibble","tidyverse","data.table","kableExtra")
+install.packages(c("readr","dplyr","ggplot2","tibble","tidyverse","data.table","kableExtra"))
